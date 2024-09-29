03edae3a98001d2cd433bc8d67757db38be21529
kbzcraft <ytkbzcraft@gmail.com>
2024-09-29 21:03:09 +0545

    it worked ../..

--- lua/hello/init.lua
+++ lua/hello/init.lua
@@ -5,7 +5,7 @@ local function hello()
 	print("Hello from the hello plugin!")
 end
 function M.setup()
-	vim.api.nvim_create_user_command("hello", hello, { range = true })
+	vim.api.nvim_create_user_command("Hello", hello, { range = true })
 end
 
 return M
